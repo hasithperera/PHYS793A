6a3db3c8ae5a16ef570fe20da99a91d980847fb0
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2022-09-28 11:15:03 -0400

    updated Q2.py with comments for the report

--- HW3/Q2.py
+++ HW3/Q2.py
@@ -9,53 +9,33 @@ Author : Hasith Perera
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 plt.figure(figsize=(4,4))
-# fig = plt.figure()
-# plt.axes(projection='polar')
-
 
 Rs = 6.957e8/(8*60*3e8) #solar radius in au
 v = 400e3/(8*60*3e8) #ms-1
 phi_0 = 0
-
-
 w_days = 27
 w = 2*np.pi/(w_days*24*3600)  #hz
 long = np.pi /2  #ecliptic plane
 
-#initial 
+#generating r range 
+r = np.arange(Rs,2,.1) 
+
+# plot arms of the spiral by
+# changing the initial location $phi_0
 for phi_d in range(0,360,40):
-    phi_shift = phi_d*np.pi/180
-    r = np.arange(Rs,2,.1) #spiral radiusnp.pi
-    phi = phi_shift + (w*np.sin(long)/v)*(Rs - r) + phi_0*np.pi/180
-    
-    # plt.plot(phi,r)
+    phi_0 = phi_d*np.pi/180
+
+    phi = phi_0 + (w*np.sin(long)/v)*(Rs - r)
+
+    #plotting each arm in a cartician axis
     h3, = plt.plot(r*np.cos(phi),r*np.sin(phi),'b-')
     
-# p1 = np.arange(0,2*np.pi,.1)
-# plt.plot(p1,1*np.ones(np.size(p1)),':k')
-# w_days = 50
-# w = 2*np.pi/(w_days*24*3600)  #hz
-v = 200e3/(8*60*3e8) #ms-1
-
-# initial 
-for phi_d in range(0,360,40):
-    phi_shift = phi_d*np.pi/180
-    r = np.arange(Rs,2,.1) #spiral radiusnp.pi
-    phi = phi_shift + (w*np.sin(long)/v)*(Rs - r) + phi_0*np.pi/180
-    h1, = plt.plot(r*np.cos(phi),r*np.sin(phi),'r-')
-    
-th = np.arange(0,2.1*np.pi,.1)
-
+#plotting the 1 AU circle
+th = np.arange(0,2.01*np.pi,.1)
 h2, = plt.plot(1*np.cos(th),1*np.sin(th),'k:')
 
 
-
-
-# h3, = plt.plot(.3*np.cos(th),.3*np.sin(th),'k:')
-    
-# modify B0
 plt.xlabel('Au')
 plt.ylabel('Au')
-plt.legend(handles=[h3,h1,h2],labels = [r'V=400 $kms^{-1}$',r'V=200 $kms^{-1}$','1AU orbit'])
+plt.legend(handles=[h3,h2],labels = ['Parker spiral','1AU orbit'])
