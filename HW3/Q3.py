94342aa80d73eac9a2372c00b30ec58f9c725398
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2022-09-30 21:51:20 -0400

    added comments to Q3

--- HW3/Q3.py
+++ HW3/Q3.py
@@ -9,13 +9,9 @@ Author : Hasith Perera
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-
 Rs = 6.957e8/(8*60*3e8) #solar radius in au
-v = 400e3/(8*60*3e8) #ms-1
+v = 700e3/(8*60*3e8) #ms-1
 phi_0 = 0
-
 
 w_days = 27
 w = 2*np.pi/(w_days*24*3600)  #hz
@@ -26,20 +22,23 @@ fig = plt.figure(figsize=(4,4))
 # syntax for 3-D projection
 ax = plt.axes(projection ='3d')
 
-#initial 
-for long in [1,90,178]:
+markers = ['o','v','^']
+
+#set longitude of the ejection point. 
+for i,long in enumerate([1,90,178]):
     
-    phi = np.arange(0,10*np.pi,.2) #spiral radiusnp.pi
+    #set up phi values 
+    phi = np.arange(0,10*np.pi,.2)
+    
+    #find spiral point for give theta and phi value
     r = Rs - v*phi/(w*np.sin(np.deg2rad(long)))
+    
     ax.plot3D(r*np.sin(np.deg2rad(long))*np.cos(phi),r*np.sin(np.deg2rad(long))*np.sin(phi),r*np.cos(np.deg2rad(long)),'o')
-    # plt.plot(r*np.cos(phi)*np.sin(np.deg2rad(long)),r*np.cos(np.deg2rad(long)),'b-')
     
     plt.figure(2,figsize=(4,4))
-    plt.plot(r*np.sin(np.deg2rad(long))*np.cos(phi),r*np.cos(np.deg2rad(long)))
+    plt.plot(r*np.sin(np.deg2rad(long))*np.cos(phi),r*np.cos(np.deg2rad(long)),':'+markers[i])
     
     
-# #modify B0
-
 plt.figure(2)
 plt.xlabel('Au')
 plt.ylabel('Au')
